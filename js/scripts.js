3fe4000f961b70a7439d627cb9c9b77b5f5445ee
Nathan Nielson <nathanbn123@gmail.com>
2019-02-01 09:53:12 -0800

    add multiple prototypes for custom pizza selection

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,38 @@
-Function newPizza(totalCost, size, ) {
-  this.totalCost = totalCost;
-  this.size = size;
+function newPizza() {
+  this.totalCost = 0;
+  this.size = ;
   this.toppings = [];
 }
 
 
 
+newPizza.prototype.selectSmall = function(){
+  this.size = ("small")
+  this.totalCost += 2;
+}
+
+newPizza.prototype.selectMedium = function(){
+  this.size = ("medium")
+  this.totalCost += 3;
+}
+
+newPizza.prototype.selectLarge = function(){
+  this.size = ("large")
+  this.totalCost += 4;
+}
 
 
-newPizza.prototype.pepperoni = function(pizza){
-  (this.toppings).push(pepperoni);
-  this.totalCost + .6;
+newPizza.prototype.addPepperoni = function(){
+  (this.toppings).push("pepperoni");
+  (this.totalCost) += .6;
+}
+
+newPizza.prototype.addOlives = function(){
+  (this.toppings).push("olives");
+  (this.totalCost) += .2;
+}
+
+newPizza.prototype.moreCheese = function(){
+  (this.toppings).push("extraCheese");
+  (this.totalCost) += .3;
 }
