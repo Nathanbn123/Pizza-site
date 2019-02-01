61d10629874632ceda9c990ee5defe8b1f41f1db
Nathan Nielson <nathanbn123@gmail.com>
2019-02-01 11:22:57 -0800

    add multiple prototypes that acutally work

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,51 @@
 function newPizza() {
   this.totalCost = 0;
-  this.size = ;
+  this.size = "";
   this.toppings = [];
 }
 
 
 
-newPizza.prototype.selectSmall = function(){
+newPizza.prototype.selectSmall = function(pizza){
   this.size = ("small")
-  this.totalCost += 2;
 }
 
-newPizza.prototype.selectMedium = function(){
+newPizza.prototype.selectMedium = function(pizza){
   this.size = ("medium")
-  this.totalCost += 3;
 }
 
-newPizza.prototype.selectLarge = function(){
+newPizza.prototype.selectLarge = function(pizza){
   this.size = ("large")
-  this.totalCost += 4;
 }
 
 
-newPizza.prototype.addPepperoni = function(){
+newPizza.prototype.addPepperoni = function(pizza){
   (this.toppings).push("pepperoni");
   (this.totalCost) += .6;
 }
 
-newPizza.prototype.addOlives = function(){
+newPizza.prototype.addOlives = function(pizza){
   (this.toppings).push("olives");
   (this.totalCost) += .2;
 }
 
-newPizza.prototype.moreCheese = function(){
+newPizza.prototype.moreCheese = function(pizza){
   (this.toppings).push("extraCheese");
   (this.totalCost) += .3;
+}
+
+newPizza.prototype.calcSizePrice = function(pizza){
+  if (this.size == ("small")) {
+    (this.totalCost) += 2;
+  } else if (this.size == ("medium")){
+    this.totalCost += 3;
+  } else if   (this.size == ("large")){
+  this.totalCost += 4;
+  }
+}
+
+newPizza.prototype.calcToppingPrice = function(pizza){
+  var toppingTotal = this.toppings.length
+  var toppingPrice = toppingTotal * .2;
+  this.totalCost += toppingPrice;
 }
