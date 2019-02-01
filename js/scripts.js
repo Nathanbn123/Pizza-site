a3f935254252fb302ca527daa2266549ffabe1c6
Nathan Nielson <nathanbn123@gmail.com>
2019-02-01 15:34:52 -0800

    add html elemnts to target with jquery

--- js/scripts.js
+++ js/scripts.js
@@ -4,29 +4,13 @@ function newPizza(totalCost, size, toppings) {
   this.toppings = toppings;
 }
 
-// newPizza.prototype.addPepperoni = function(pizza){
-  //   (this.toppings).push("pepperoni");
-  //   (this.totalCost) += .3;
-  // }
-  //
-  // newPizza.prototype.addOlives = function(pizza){
-    //   (this.toppings).push("olives");
-    //   (this.totalCost) += .2;
-    // }
-    //
-    // newPizza.prototype.moreCheese = function(pizza){
-      //   (this.toppings).push("extraCheese");
-      //   (this.totalCost) += .1;
-      // }
-
-
 newPizza.prototype.calcSizePrice = function(){
   if (this.size == ("small")) {
-    (this.totalCost) += 2;
+    (this.totalCost) += 3;
   } else if (this.size == ("medium")){
-    this.totalCost += 3;
-  } else if   (this.size == ("large")){
     this.totalCost += 4;
+  } else if   (this.size == ("large")){
+    this.totalCost += 5;
   }
   return this.totalCost
 }
@@ -43,16 +27,6 @@ newPizza.prototype.calcToppingPrice = function(){
 }
 
 
-
-// function submitToppings(input){
-//   pizzo.toppings.push(input);
-//   console.log(pizzo.toppings)
-//   pizzo.toppings.calcToppingPrice();
-//   console.log(pizzo)
-//   }
-
-
-
 $(document).ready(function() {
   $("form.pizza-order").submit(function(event) {
     event.preventDefault();
@@ -61,12 +35,14 @@ $(document).ready(function() {
     $("input:checkbox[name=topping]:checked").map(function(toppings){
       toppingArray[toppings] = $(this).val();
     });
-
       var pizzo = new newPizza(0, size, toppingArray )
       console.log(pizzo)
       pizzo.calcSizePrice();
       console.log(pizzo)
       pizzo.calcToppingPrice();
       console.log(pizzo)
+
+      $('#pizza-size').text(pizzo.size);
+
  });
 });
